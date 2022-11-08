7df138934eb3511333ee4b6918574b4c8d0a599e
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-11-08 20:21:34 +0200

    Added icon and title to window frame

--- src/main/java/edu/hh/dashboard/screens/WindowFrame.java
+++ src/main/java/edu/hh/dashboard/screens/WindowFrame.java
@@ -4,13 +4,17 @@ import javax.swing.*;
 
 public class WindowFrame extends JFrame {
     private JPanel currentPanel;
+    private ImageIcon appIcon = new ImageIcon(Main.class.getResource("/Amandan_uusi_logo_png.png"));
 
-    public WindowFrame(){
-        this.setSize(600,400);
+    public WindowFrame() {
+        this.setSize(600, 400);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Magic fitting room - Dashboard");
+        this.setIconImage(appIcon.getImage());
+
         currentPanel = new MainPanel();
         this.add(currentPanel);
         this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 
